b9e94c53e3a6e39e139da8fa9e74ed8322322a75
whatislove12 <vlad_podymskiy@mail.ru>
2023-01-19 19:53:09 +0100

    add informaition of 19.01

--- mod-js.es/scripts/objeto-programador.js
+++ mod-js.es/scripts/objeto-programador.js
@@ -1,14 +1,19 @@
 var productos = [];
 
     window.onload = function(ev) {
-        var producto = new Producto('CMC001','Camiseta manga corta Mislata Center',10.99);
+        var producto = null; /* new Producto('CMC001','Camiseta manga corta Mislata Center',10.99); */
 
-        producto.discripcion = 'Camiseta de algodon corte unisex en varios colores de manga corta con el logotipa del centro';
-        producto.minStock = 5;
+
+        for (let i=0;i<=20;i++) {
+        producto = new Producto('CMC0', + String(i).padStart(2,'0') +1, 'Camiseta manga corta Mislata Center',10.99);
+        producto.descripcion = 'Camiseta de algodon corte unisex en varios colores de manga corta con el logotipa del centro';
+        producto.setMinStock = 5;
         producto.iva = 0.21;
         producto.modificarPrecio(-10)
-        producto.stock = 3;
-
-        console.log(producto);
+        producto.stock = Math.floor(Math.random()*20);
         productos.push(producto);
-    }
+    };
+        for (let i=0;i<productos.length;i++) {
+            document.getElementById('lista').innerHTML += productos[i].fichaProducto('div');
+        }
+    };
--- mod-js.es/scripts/producto.js
+++ mod-js.es/scripts/producto.js
@@ -1,31 +1,65 @@
 class Producto {
 
 // Propiedades 
-    id = '';
+    __id = 0;
     nombre = '';
-    discripcion = '';
+    descripcion = '';
     stock = null;
-    minStock = 0;
-    maxStock = 0;
+    __stock = 0;
+    __minStock = 0;
+    get getMinStock(){}
+    set setMinStock(valor) {}
+    __maxStock = 0;
     precio = null;
     iva = 0.0;
-    fechaAlta = null;
     activo = true;
+    fechaAlta = null;
+
 
     // metodo constructor 
-    constructor(id,nombre,precio,stock) {
-        this.id = id;
+    constructor(nombre,precio,stock) {
+        this.__id = ++Producto.contador;
         this.nombre = nombre;
         this.precio = precio || 0.0;
-        this.stock = stock || 0;
+        this.setStock = stock || 0;
         this.fechaAlta = new Date();
-        this.maxStock = 50;
+        this.__maxStock = 50;
+        this.__minStock = 0;
 
     }
 
+    
+    get getMinStock() {
+        return this.__minStock;
+    }
+    
+    set setMinStock(valor){
+        if (valor>=0 && valor<=this.getMaxStock)
+        this.__minStock=valor;
+    }
+    
+    get getMaxStock() {
+        return this.__maxStock;
+    }
+    
+    set setMaxStock(valor) {
+        if (valor>=this.getMinStock)
+        this.__maxStock=valor;
+    }
+    
+    get getStock() {
+        return this.__stock ;
+    }
+    
+    set setStock(valor) {
+        if (valor>=this.getMinStock && valor<=this.getMaxStock)
+        this.__stock=valor;
+    }
+    
+
     // METODOS 
     disponible() {
-        return this.activo && this.stock>0;
+        return this.activo && this.getStock>0;
     }
 
     modificarPrecio(porcentaje) {
@@ -34,16 +68,63 @@ class Producto {
             this.precio = Math.round((this.precio*(1+porcentaje))*100)/100;
     }
 
+    obtenerIVA() {
+        return Math.round((this.precio*this.obtenerIVA)*100)/100;
+
+    }
+
     precioConIva() {
-        return Math.round((this.precio*(1+this.iva))*100)/100;
+        return this.precio + this.obtenerIVA;
     }
 
     seNecesitaStock() {
-        return this.activo && this.stock<this.minStock
+        return this.activo && this.getStock<this.getMinStock
     }
 
     unidadesASolitades() {
         if (!this.seNecesitaStock()) return 0;
-        else return this.maxStock - this.stock;
+        else return this.getMaxStock - this.getStock;
+    }
+
+    fichaProducto(etiqueta,tipoLista,clases){
+        var html='';
+        tipoLista = tipoLista || 'ul';
+        clases = clases || '';
+        if (this.esValida('tag',etiqueta) && this.esValida('list',tipoLista)) {
+            html += '<' +etiqueta;
+            if (clases.length>0 || this.activo || this.seNecesitaStock()) {
+                let clasesAdd = '';
+                clasesAdd += clases.length>0?clases:'';
+                clasesAdd += !this.activo?' noActivo':'';
+                clasesAdd += this.seNecesitaStock()?' sinStock':'';
+                html += ' class="' +clasesAdd.trim()+'"';
+            }
+            html +='>';
+
+            html += '<h2>'+this.id+' - '+this.nombre+'</h2>'
+            html += '<'+tipoLista+'>';
+            html += '<li><b>Precio:</b>'+this.precio+'</li>';
+            html += '<li><b>IVA:</b>'+this.obtenetIVA+'</li>';
+            html += '<li><b>Total:</b>'+this.precioConIva()+'</li>';
+            html += '<li><b>Stock:</b>'+this.getStock+'</li>';
+            html += '<li><b>Stock:</b>'+this.getMinStock+'</li>';
+            html += '<li><b>Stock:</b>'+this.getMaxStock+'</li>';
+
+            html += '<li><b>Activo:</b>'+this.activo+'</li>';
+
+            html += '<li><b>Reponer:</b>'+(this.seNecesitaStock()?'SI('+this.unidadesASolitades()+')':'NO')+'</li>';
+
+
+            html += '<li><b>Descripcion:</b>'+this.descripcion+'</li>';
+            html += '<'+tipoLista+'>';
+            html += '</' +etiqueta+'>';
+
+        }
+
+        return html;
+    }
+
+    esValida(tipoValidacion,valor) {
+        return true;
     }
 }
